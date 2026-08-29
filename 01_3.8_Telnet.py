c7b63b43d6eba76eddedb79a83f562c3888f9c11
jakir025 <jakir25@gmail.com>
2026-08-29 18:03:57 +0530

    Update 01_3.8_Telnet.py

--- 01_3.8_Telnet.py
+++ 01_3.8_Telnet.py
@@ -2,9 +2,9 @@ from telnetlib import Telnet
 
 # Use input() for Python 3 (raw_input() was removed in Python 3)
 cmd = input('Enter the command : ')
-tn = Telnet('192.168.184.141')   # connect to finger port
-tn.write(b'jakir\r\n')
-tn.write(b'Hoss1234\r\n')  
+tn = Telnet('192.168.XX.XX')   # connect to finger port
+tn.write(b'XXXX\r\n')
+tn.write(b'XXX\r\n')  
 tn.write(b'term length 0\n')
 # Fixed: removed bad backslash and added proper byte string concatenation with newline
 tn.write(cmd.encode('ascii') + b'\n')
